6289eaf73bd91c1525c7c5e26b1dc7b0a104d6b5
Yatheswar546 <yatheswar546@gmail.com>
2026-04-28 00:32:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        <h1>Counter App</h1>
+
+        <p>Count: {this.state.count}</p>
+
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default App
